a85eb33a5a9a4003c1a3d524d4c1fa73011124ac
Matthieu Canet <mattcanet@gmail.com>
2022-06-12 22:55:04 +0200

    Initial commit

---
+++ scripts/functions.js
@@ -0,0 +1,88 @@
+function ingredientsList(recipes) {
+	let ingredientsArray = [];
+	let l = 0;
+
+	//Parcours de toutes les recettes
+	for (let i = 0; i < recipes.length; i++) {
+		//Parcours de tous les ingrédients de chaque recette
+		for (let j = 0; j < recipes[i][1].ingredients.length; j++) {
+			//Filtre du tableau des ingrédients
+			const result = ingredientsArray.filter(
+				ingredient =>
+					ingredient.toLowerCase() ===
+					recipes[i][1].ingredients[j].ingredient.toLowerCase(), //Permet de comparer les chaînes de caractères en minuscule
+			);
+
+			//Si l'ingrédient n'existe pas déjà dans la liste, on l'ajoute
+			if (result.length == 0) {
+				ingredientsArray[l] = recipes[i][1].ingredients[j].ingredient;
+				l = l + 1;
+			}
+		}
+	}
+	console.log(ingredientsArray);
+	return ingredientsArray;
+}
+
+//function appareils();
+function appliancesList(recipes) {
+	let appliancesArray = [];
+	let l = 0;
+
+	//Parcours de toutes les appareils
+	for (let i = 0; i < recipes.length; i++) {
+		//Filtre du tableau des appareils
+		const result = appliancesArray.filter(
+			appliance =>
+				appliance.toLowerCase() ===
+				recipes[i][1].appliance.toLowerCase(), //Permet de comparer les chaînes de caractères en minuscule
+		);
+
+		//Si l'appareil n'existe pas déjà dans la liste, on l'ajoute
+		if (result.length == 0) {
+			appliancesArray[l] = recipes[i][1].appliance;
+			l = l + 1;
+		}
+	}
+	console.log(appliancesArray);
+	return appliancesArray;
+}
+
+function ustensilsList(recipes) {
+	let ustensilsArray = [];
+	let l = 0;
+
+	//Parcours de toutes les recettes
+	for (let i = 0; i < recipes.length; i++) {
+		//Parcours de tous les ustenciles de chaque recette
+		for (let j = 0; j < recipes[i][1].ustensils.length; j++) {
+			//Filtre du tableau des ustenciles
+			const result = ustensilsArray.filter(
+				ustensil =>
+					ustensil.toLowerCase() ===
+					recipes[i][1].ustensils[j].toLowerCase(), //Permet de comparer les chaînes de caractères en minuscule
+			);
+
+			//Si l'ustencile n'existe pas déjà dans la liste, on l'ajoute
+			if (result.length == 0) {
+				ustensilsArray[l] = recipes[i][1].ustensils[j];
+				l = l + 1;
+			}
+		}
+	}
+	console.log(ustensilsArray);
+	return ustensilsArray;
+}
+
+function showIngredients(recipes) {
+	let text = '';
+	if (recipes.unit != null) {
+		text =
+			recipes.ingredient + ': ' + recipes.quantity + ' ' + recipes.unit;
+	} else if (recipes.quantity != null) {
+		text = recipes.ingredient + ': ' + recipes.quantity;
+	} else {
+		text = recipes.ingredient;
+	}
+	return text;
+}
---
+++ scripts/index.js
@@ -0,0 +1,82 @@
+import { recipes } from '../modules/recipes.js';
+let recipesArray = Object.entries(recipes);
+
+console.log(recipesArray);
+
+// INSERT ELEMENT IN HTML
+function create_article(recipesArray) {
+	for (let i = 0; i < recipesArray.length; i++) {
+		const article = document.createElement('article');
+		article.classList.add('recipes');
+		article.id = 'recipes' + i;
+		document.querySelector('.all_recipes').appendChild(article);
+
+		const recipes_img = document.createElement('img');
+		recipes_img.classList.add('recipes_img');
+		recipes_img.id = 'recipes_img' + i;
+		document.getElementById('recipes' + i).appendChild(recipes_img);
+
+		const div_recipes = document.createElement('div');
+		div_recipes.classList.add('recipes_text');
+		div_recipes.id = 'recipes_text' + i;
+		document.getElementById('recipes' + i).appendChild(div_recipes);
+
+		const div_first_part = document.createElement('div');
+		div_first_part.classList.add('first_part');
+		div_first_part.id = 'div_first_part' + i;
+		document.getElementById('recipes_text' + i).appendChild(div_first_part);
+
+		const recipes_title = document.createElement('h1');
+		recipes_title.classList.add('recipes_title');
+		recipes_title.innerHTML = recipesArray[i][1].name;
+		document
+			.getElementById('div_first_part' + i)
+			.appendChild(recipes_title);
+
+		const recipes_clock = document.createElement('i');
+		recipes_clock.className = 'far fa-clock';
+		recipes_clock.innerHTML = recipesArray[i][1].time + ' min';
+		document
+			.getElementById('div_first_part' + i)
+			.appendChild(recipes_clock);
+
+		const div_second_part = document.createElement('div');
+		div_second_part.classList.add('second_part');
+		div_second_part.id = 'recipes_ingredient' + i;
+		document
+			.getElementById('recipes_text' + i)
+			.appendChild(div_second_part);
+
+		const ul_ingredients = document.createElement('ul');
+		ul_ingredients.id = 'list_ingredient' + i;
+		ul_ingredients.classList.add('list_ingredient');
+		document
+			.getElementById('recipes_ingredient' + i)
+			.appendChild(ul_ingredients);
+
+		for (let j = 0; j < recipesArray[i][1].ingredients.length; j++) {
+			const ingredients = document.createElement('li');
+			ingredients.classList.add('ingredients_recipes');
+			ingredients.innerHTML = showIngredients(
+				recipesArray[i][1].ingredients[j],
+			);
+			document
+				.getElementById('list_ingredient' + i)
+				.appendChild(ingredients);
+		}
+		const description = document.createElement('p');
+		description.classList.add('description_recipes');
+		description.innerText = recipesArray[i][1].description;
+		document
+			.getElementById('recipes_ingredient' + i)
+			.appendChild(description);
+	}
+}
+
+create_article(recipesArray);
+
+ingredientsList(recipesArray);
+
+ustensilsList(recipesArray);
+
+appliancesList(recipesArray);
